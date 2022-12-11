c984d43dfb43e3beb5392a2159d2af7e99acaf8c
Irene Antelo <iantelovazquez@gmail.com>
2022-12-11 20:20:56 +0100

    last changes

---
+++ script.js
@@ -0,0 +1,129 @@
+const balance = document.getElementById("balance");
+const money_plus = document.getElementById("money-plus");
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+const money_minus = document.getElementById("money-minus");
+
+const localStorageTransations = JSON.parse(localStorage.getItem("transations"));
+
+let transations =
+  localStorage.getItem("transations") !== null ? localStorageTransations : [];
+
+//add transation
+function addTransation(e) {
+  e.preventDefault();
+  if (text.value.trim() === "" || amount.value.trim() === "") {
+    text.placeholder = "please add a description";
+    text.style.backgroundColor = "#ccc";
+    amount.placeholder = "please add amount";
+    amount.style.backgroundColor = "#ccc";
+  } else {
+    const transation = {
+      id: genenrateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
+    transations.push(transation);
+    addTransationDOM(transation);
+    updateValues();
+    updateLocalStorage();
+    text.value = "";
+    amount.value = "";
+  }
+}
+//generate id
+function genenrateID() {
+  return Math.floor(Math.random() * 100000000);
+}
+
+//add transations to dom list
+function addTransationDOM(transation) {
+  //get sign
+  const sign = transation.amount < 0 ? "-" : "+";
+  const item = document.createElement("li");
+  //add class based on value
+  item.classList.add(transation.amount < 0 ? "minus" : "plus");
+  item.innerHTML = `${transation.text} <span>${sign}${Math.abs(
+    transation.amount
+  )}<button class="delete-btn" onclick="removeTransation(${
+    transation.id
+  })">x</button>`;
+  list.appendChild(item);
+}
+
+//update the balance, income and expense
+function updateValues() {
+  const amounts = transations.map((transation) => transation.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerText = `${total}€`;
+  money_plus.innerText = `${income}€`;
+  money_minus.innerText = `${expense}€`;
+}
+
+function showHideList () {
+  const transactionList = document.getElementById("list");
+  transactionList.style.display = "none";
+  const checkedbox = document.getElementById("show-hide")
+  if (checkedbox.checked) {
+    transactionList.style.display = "block";
+  }
+  else {
+    transactionList.style.display = "none";
+  }
+}
+
+
+//remove
+function removeTransation(id) {
+  transations = transations.filter((transation) => transation.id !== id);
+  updateLocalStorage();
+  init();
+}
+
+
+//updatelocal storage
+function updateLocalStorage() {
+  localStorage.setItem("transations", JSON.stringify(transations));
+}
+
+//init
+function init() {
+  list.innerHTML = "";
+  transations.forEach(addTransationDOM);
+  updateValues();
+}
+init();
+
+form.addEventListener("submit", addTransation);
+
+function ask() {
+  if (confirm('Clear localStorage?') == true) {
+  localStorage.clear()
+  location.reload()
+  }
+  else {
+  alert('Nothing happend')
+  }
+  }
+
+  
+
+
+
+
+
+
+
+
+
